ddb0c31744b4a89aba287e793c625d44c90e5a5b
Allan Jonatan <jonatan.allan@hotmail.com>
2023-12-21 16:36:12 -0300

    feat: BackgroundService with PeriodicTimer

---
+++ src/CaliberFS.Template.Worker/Configuration/HealthCheckConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using CaliberFS.Template.IoC.DependencyInjection;
+
+namespace CaliberFS.Template.Worker.Configuration
+{
+    public static class HealthCheckExtensions
+    {
+        public static IServiceCollection AddCustomHealthCheck(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddHealthChecks()
+                .AddSqlServerHealthCheck()
+                .AddRabbitMQHealthCheck();
+            return services;
+        }
+    }
+}
